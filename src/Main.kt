30c9f00df6c1a0f5a13d31888951e0f23bc1278b
merna <merna@MERNA_EMEL>
2025-07-21 09:50:57 +0300

    Add Basic task manager

---
+++ src/Main.kt
@@ -0,0 +1,49 @@
+
+//fun main(){
+//    println("Setup successful!")
+//}
+fun main(){
+////// Create a data class Task
+data class Task(val title: String, val isDone : Boolean )
+
+////// Declare a mutable List of tasks
+val tasks = mutableListOf(
+ Task("Submit assignment", true) ,
+ Task("Clean the house"  , false),
+ Task("Attended the live session",true))
+
+////// Loop through the tasks and print thier status using a conditional:
+for(task in tasks){
+ if(task.isDone==true) {
+     println("✅ ${task.title}")
+ }
+ else {
+     println("❌ ${task.title}")
+   }
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
